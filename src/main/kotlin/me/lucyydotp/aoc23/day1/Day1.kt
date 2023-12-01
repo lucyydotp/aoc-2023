9cdf5f8a6da33b1a455e61bb47dda1966f549617
Lucy Poulton <lucy@lucypoulton.net>
2023-12-01 09:30:12 +0000

    there's a standard library method for that

--- src/main/kotlin/me/lucyydotp/aoc23/day1/Day1.kt
+++ src/main/kotlin/me/lucyydotp/aoc23/day1/Day1.kt
@@ -3,9 +3,7 @@ package me.lucyydotp.aoc23.day1
 import me.lucyydotp.aoc23.Utils.plus
 
 object Day1 {
-
-    private const val DIGITS = "0123456789"
-    private fun parseLine(line: String): Int = (line.first { it in DIGITS } + line.last { it in DIGITS }).toInt()
+    private fun parseLine(line: String): Int = (line.first(Char::isDigit) + line.last(Char::isDigit)).toInt()
     fun calculate(input: String) = input
         .lines()
         .filterNot(String::isBlank)
